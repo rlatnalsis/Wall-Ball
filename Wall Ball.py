8affb53da34064ad51b16095992bf80d1111f1ba
Sumin <rlatnals5006@gmail.com>
2021-08-29 18:17:05 +0900

    v.4.0.0    event processing

--- Wall Ball.py
+++ Wall Ball.py
@@ -1,68 +1,26 @@
-# Unit PYG04: Pygame Wall Ball Game --version 6
+# Unit PYG04: Pygame Wall Ball Game_event processing
 import pygame, sys
 
 pygame.init()
-size = width, height = 600, 400
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-BLACK = 0, 0, 0
-
+screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("Wall Ball Game")
-icon = pygame.image.load("PYG03_flower.png")
-pygame.display.set_icon(icon)
-
-ball = pygame.image.load("PYG02_ball.gif")
-ballrect = ball.get_rect()
-speed = [1, 1]
 fclock = pygame.time.Clock()
-fps = 300
-still = False
+fps = 1
+num = 1
 
 while True:
+    uevent = pygame.event.Event(pygame.KEYDOWN, {"unicode": 123, "key": pygame.K_SPACE, "mod": pygame.KMOD_ALT})
+    pygame.event.post(uevent)
+    num = num + 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0]) - 1) * int(speed[0] / abs(speed[0]))
-            elif event.key == pygame.K_RIGHT:
-                speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
-            elif event.key == pygame.K_UP:
-                speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
-            elif event.key == pygame.K_DOWN:
-                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1]) - 1) * int(speed[1] / abs(speed[1]))
-
-            elif event.key == pygame.K_ESCAPE:
-                sys.exit()
-
-        elif event.type == pygame.VIDEORESIZE:
-            size = width, height = event.size[0], event.size[1]
-            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                still = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            still = False
-            if event.button == 1:
-                ballrect = ballrect.move(event.pos[0] - ballrect.left, event.pos[1] - ballrect.top)
-        elif event.type == pygame.MOUSEMOTION:
-            if event.buttons[0] == 1:
-                ballrect = ballrect.move(event.pos[0] - ballrect.left, event.pos[1] - ballrect.top)
-
-    if pygame.display.get_active() and not still:
-        ballrect = ballrect.move(speed)
-
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = - speed[0]
-        if ballrect.right > width and ballrect.right + speed[0] > ballrect.right:
-            speed[0] = - speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = - speed[1]
-        if ballrect.bottom > height and ballrect.bottom + speed[1] > ballrect.bottom:
-            speed[1] = - speed[1]
+            if event.unicode == "":
+                print("[KEYDOWN {}]:".format(num), "#", event.key, event.mod)
+            else:
+                print("[KEYDOWN {}]:".format(num), event.unicode, event.key, event.mod)
 
     pygame.display.update()
-    screen.fill(BLACK)
-    screen.blit(ball, ballrect)
     fclock.tick(fps)
