0c1cf9c6813db415f88375e97a1a88312393caf2
Sumin <rlatnals5006@gmail.com>
2021-08-31 04:26:07 +0900

    v.6.3.0    draw font as a surface

--- Wall Ball.py
+++ Wall Ball.py
@@ -1,4 +1,4 @@
-# Unit PYG05: Pygame Wall Ball Game_draw font
+# Unit PYG05: Pygame Wall Ball Game_draw font as a surface
 import pygame, sys
 import pygame.freetype
 
@@ -8,10 +8,12 @@ pygame.display.set_caption("Wall Ball Game")
 GOLD = 255, 215, 0
 
 f1 = pygame.freetype.Font('C:\Windows\Fonts\Gabriola.ttf', 36)
-f1rect = f1.render_to(screen, (200, 160), "draw font", fgcolor=GOLD, size=50)
+f1surf, f1rect = f1.render("draw font", fgcolor=GOLD, size=50)
+f1rect = f1.render_to(screen, (200, 100), "draw font", fgcolor=GOLD, size=50)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    screen.blit(f1surf, (200, 160))
     pygame.display.update()
